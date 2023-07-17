8b2a7b8980904bbb4d3452516e4a27d5c2843010
feelswhiteman <feelswhiteman@gmail.com>
2023-07-17 21:54:08 +0300

    Added few conersation states, defined ChatInfo type

--- telegram-bot/src/types.ts
+++ telegram-bot/src/types.ts
@@ -1,6 +1,20 @@
-export type ConversationState = "DEFAULT" | "AWAITING_USERNAME" | "WAITING_FOR_PARTNER";
+export type ConversationState =
+    | "DEFAULT"
+    | "AWAITING_PARTNER_INFORMATION"
+    | "WAITING_FOR_PARTNER"
+    | "WAITING_FOR_CONFIRMATION"
+    | "WAITING_FOR_CONVERSATION_TO_START"
+    | "CONNECTED";
+
 export type Username = `@${string}`;
 
-export const isUsername = (str: string): str is Username  => {
-    return str.startsWith('@');
+export interface ChatInfo {
+    id?: number | string;
+    username?: Username;
+    first_name?: string;
+    last_name?: string;
 }
+
+export const isUsername = (str: string): str is Username => {
+    return str.startsWith("@");
+};
